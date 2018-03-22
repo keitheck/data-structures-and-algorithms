8c2aaeccdbf8e3682c92f6b6ef5ccc80029de699
keitheck <eckertkeith@gmail.com>
2018-03-22 15:45:04 -0700

    fixed tests and docstrings

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,6 +1,10 @@
 
 
 def find_index(list, key):
+    """
+    This function accepts a sorted list of ints of unknown length and
+    a key value.  Will return the index of matched key.
+    """
     count = 0
     for index in list:
         if index == key:
--- binary-search/test_binary_search.py
+++ binary-search/test_binary_search.py
@@ -1,15 +1,20 @@
 import binary_search as binary_search
-import pytest as pytest
+# import pytest as pytest
 
 
 def test_find_index():
     # test is key is in list
     assert binary_search.find_index([1, 2, 3, 4, 5], 5) is 4
+
+
+def test_not_in_list():    
     # test that key is not in list
     assert binary_search.find_index([22, 34, 55, 67, 89, 320, 675, 12233], 11) is -1
-    # test that list is empty
-    assert binary_search.find_index([], 1) is -1
     sample_list = list(range(3, 100000, 13))
     assert binary_search.find_index([sample_list], 4557) is -1
-    
+
+
+def test_empty_list():    
+    # test that list is empty
+    assert binary_search.find_index([], 1) is -1
 
