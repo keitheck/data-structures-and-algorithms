f54a2a6c5b01501bec7619a2626287a9733e4c26
keitheck <eckertkeith@gmail.com>
2018-03-26 14:55:59 -0700

    tests added

---
+++ shift-array/test_shift_array.py
@@ -0,0 +1,13 @@
+import shift_array as sa
+
+
+def test_new_list_inserts_correctly_even():
+    assert sa.new_list([2, 3, 4, 5], 6) == [2, 3, 6, 4, 5]
+
+
+def test_new_list_inserts_correctly_odd():
+    assert sa.new_list([2, 3, 4, 5, 6], 7) == [2, 3, 4, 7, 5, 6]  
+
+
+def test_insert_non_int():
+    assert sa.new_list([1, 2, 3, 4, 5, 6], 'a') == [1, 2, 3, 'a', 4, 5, 6]   
