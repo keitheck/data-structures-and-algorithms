22da3149eca8b7593416c5590a7ec17266172303
keitheck <eckertkeith@gmail.com>
2018-04-02 14:32:10 -0700

    queue fixed

--- data_structures/queue/queue.py
+++ data_structures/queue/queue.py
@@ -6,58 +6,64 @@ class Queue:
     def __init__(self, iterable=[]):
         """runs on initiation of class Queue"""
         self.back = None
-        self.front = None
         self._size = 0
         
         for item in iterable:
             print(item)
             self.enqueue(item)
-        # try:
-        # except TypeError:
-        #     print('Pleae instansiate with an iterable')
             
     def __len__(self):
         """magic method that returns length of the queue"""
         return self._size
 
+    def __repr__(self):
+        """technical representation"""
+        current = self.back
+        while current._next:
+            current = current._next
+        print("node at front of queue => {}".format(current.val))
+
     def __str__(self):
-        """return the value of front of the queue"""
+        """return the value of back of the queue"""
         try:
-            q_front = self.front.val
-            print("Front")
+            print("Back of Queue => {}".format(self.back))
         except ValueError:
             print('Queue is empty')
     
     def enqueue(self, val):
         """add a value to the back of the queue with an O(1) time"""
         print('fired')
-        # node = Node(val)
 
         if self.back:
-            self.back._next = Node(val)
-            self.back = self.back._next
+            self.back = Node(val, self.back)
 
         else:
-            print('Changed:')
+            print('else: self.back = Node(val)')
             self.back = Node(val)
-            print('self.back = Node(val)')
-            self.front = self.back
-            print('self.front = self.back')
 
         self._size += 1
         print('self.back = self.back._next.Node(val)')
 
     def dequeue(self):
         """removes and returns node value from front of queue"""
-        dequeue = None
-        if self.front is None:
+        
+        if self.back is None:
             raise ValueError('No value to rueturn, Queue is empty')
-        else:
-            print('dequeval fired')
-            dequeval = self.front
-            self.front = self.back
-            self.back = self.back._next
+
+        if self.back._next is None:
+            x = self.back
+            self.back = None
             self._size = 0
-            return dequeval.val
+            return x.val    
+
+        dequeue = self.back
+
+        while dequeue._next:
+            prev = dequeue
+            dequeue = dequeue._next
+            
+        prev._next = None
+        self._size -= 1
+        return dequeue.val
 
 
