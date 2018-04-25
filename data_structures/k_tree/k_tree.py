e288f0308447c4194fc452ff6c5d4e25b708c0d7
keitheck <eckertkeith@gmail.com>
2018-04-25 08:56:39 -0700

    pre/post order added

--- data_structures/k_tree/k_tree.py
+++ data_structures/k_tree/k_tree.py
@@ -1,14 +1,14 @@
 class Node:
     """defines each node used to build the k-ary tree"""
-    def __init__(self, val, children=None):
+    def __init__(self, val, input_list=[]):
         self.val = val
-        if children is None:
-            self.children = []
-        self.children = children
+        self.children = []
+        for child in input_list:
+            self.children.append(Node(child))
 
     def __repr__(self):
         # return 'node children: {} '.format(lambda x: for x in self.children: return x.val)
-        return self.root.val 
+        return str(self.val) 
 
     def __str__(self):
         return 'node value: {}'.format(self.val)
@@ -16,32 +16,33 @@ class Node:
 
 class Ktree:
     """defines k-ary tree"""
-    def __init__(self, val, children=[]):
+    def __init__(self, val, input_list=[]):
         """defines root node"""
-        node = Node(val, children)
+        # if input_list is not list:
+        #     return KeyError
+        node = Node(val, input_list)
         self.root = node
 
     def __repr__(self):
         """returns a string of type and address"""
         # return 'node children: {} '.format(lambda x: for x in self.children: return x.val)
-        return '<{} at {}>'.format(type(self, hex(id(self))))
+        # return '<{} at {}>'.format(type(self, hex(id(self))))
 
     def __str__(self):
         """string representation of all nodes values"""
         return 'root value: {}'.format(self.root.val)
 
     def pre_order(self, operation=None):
-        """performs operation in pre-order traversal"""
-        po_list = []
+        """returns node values in a pre-order traversal
+           takes an argument operation in the form of a lambda function         
+        """
 
         def _walk(node=None):
             if node is None:
                 return
-            print('operation')
 
-            import pdb ; pdb.set_trace()
-            po_list.append(node.val)
-
+            operation(node)
+            
             for child in node.children:
                 _walk(child)
         
@@ -50,49 +51,29 @@ class Ktree:
         else:
             _walk(self.root)    
 
+    def post_order(self, operation=None):
+        """
+        returns node values in a pre-order traversal
+        takes an argument operation in the form of a lambda function         
+        """
+        
+        def _walk(node=None):
+            if node is None:
+                return
 
+            for child in node.children:
+                _walk(child)
+        
+            operation(node)
 
+        if self.root is None:
+            return False
+        else:
+            _walk(self.root)  
 
-
-    
-    
-    # def insert(self, parent_val, val=None):
-    #     """function that inserts node into tree"""
-    #     node = Node(val)
-    #     current = self.root
-
-    #     if self.root.val is None:
-    #         self.root = node
-    #         return self.root.val
-
-    #     else:
-    #         def _search(self, parent_val, node):
-    #             if parent_val == current.val:
-    #                 current.children.append(node)
-    #                 return '{} added to {}'.format(node, parent_val)
-    #             for item in node.children:
-    #                 _search(item, parent_val, node)    
-
-    # def pre_order(self, operation=None):
-    #     def _walk(node=None):
-    #         if not Node:
-    #             return
-    #         operaton(node.val)
-    #         for item in node.children:
-    #             _walk(item) 
-    #     _walk(self.root)           
-
-    # def post_order(self, operation=None):
-    #     """function that searches ktree"""
-    #     def _walk(node=None):
-    #         if not Node:
-    #             return
-    #         for item in node.children:
-    #             _walk(item) 
-    #         operaton(node.val)
-    #     _walk(self.root)
-
-    # def breadth_first(self):
-    #     pass
-
-                
+    def breadth_first(self, operation=None):
+        """
+        returns node valuse in beadth first traversal.
+        takes an argument operation in the form of a lambda function
+        """
+        pass
